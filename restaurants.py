6f2c27e8c31be6a6ef86e23e1fbc3fc300382383
FloritaZhao <feifeilan@hotmail.com>
2025-06-05 11:06:29 -0700

    updated

--- restaurants.py
+++ restaurants.py
@@ -1,6 +1,26 @@
+from collections import defaultdict
+
 def most_varied_visitor(visits):
     # Write your solution here!
-    pass
+    # import defaultdict(set)
+    most_visitor = defaultdict(set)
+
+    for restaurant, people in visits.items():
+        for person in people:
+            most_visitor[person].add(restaurant)
+
+    print(type(most_visitor))
+
+    max_count = 0
+    most_person = None
+    for person, restaurant in most_visitor.items():
+        if len(restaurant) > max_count:
+            max_count = len(restaurant)
+            most_person = person
+    
+    return most_person
+
+
 
 
 visits_1 = {
@@ -8,6 +28,10 @@ visits_1 = {
     "La Especial Norte" : ["Eliza"],
     "Sushi Kashiba": ["Xinting"],
 }
+
+most_varied_visitor(visits_1)
+print(most_varied_visitor(visits_1))
+
 assert most_varied_visitor(visits_1) == "Eliza"
 
 visits_2 = {
@@ -17,6 +41,8 @@ visits_2 = {
     "Lyon's Grocery": ["Auberon", "Xinting", "Sam", "Xinting"],
     "Applebee's": ["Sam", "Eliza"],
 }
+
+
 assert most_varied_visitor(visits_2) == "Auberon"
 
 visits_3 = {
